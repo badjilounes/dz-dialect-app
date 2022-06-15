7e77bbf9ada3a94d9534828e91722df15a6a0673
Lounès Badji <lounes.badji.ext@agicap.com>
2022-06-15 02:16:38 +0200

    fix: error handling on test home server issue

--- src/app/pages/test/test-home/test-home.component.ts
+++ src/app/pages/test/test-home/test-home.component.ts
@@ -1,8 +1,9 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { BehaviorSubject, map, Observable, shareReplay, tap } from 'rxjs';
+import { BehaviorSubject, catchError, EMPTY, map, Observable, shareReplay, tap } from 'rxjs';
 import { SentenceControllerHttpService } from 'src/clients/dz-dialect-api';
 import { TestService } from '../test.service';
 
@@ -25,6 +26,7 @@ export class TestHomeComponent {
     private readonly router: Router,
     private readonly api: SentenceControllerHttpService,
     private readonly testService: TestService,
+    private readonly snackBar: MatSnackBar,
   ) {}
 
   startTraining(): void {
@@ -35,6 +37,11 @@ export class TestHomeComponent {
         tap((sentences) => this.testService.init(sentences)),
         tap(() => this.loading$.next(false)),
         tap(() => this.router.navigate(['/test/start'])),
+        catchError((error) => {
+          this.loading$.next(false);
+          this.snackBar.open(error.message, 'OK', { duration: 3000 });
+          return EMPTY;
+        }),
         untilDestroyed(this),
       )
       .subscribe();
