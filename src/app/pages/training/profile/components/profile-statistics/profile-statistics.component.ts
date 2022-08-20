b68347c13f5f346bafffc5dc0d7b8d4bf39acd97
Lounes Badji <lounes.b.ext@padoa-group.com>
2022-08-21 00:05:10 +0200

    feat(profile): css improvements + button show all

--- src/app/pages/training/profile/components/profile-statistics/profile-statistics.component.ts
+++ src/app/pages/training/profile/components/profile-statistics/profile-statistics.component.ts
@@ -1,17 +1,19 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {TranslateModule} from "@ngx-translate/core";
-import {ProfileCardComponent} from "../profile-card/profile-card.component";
-import {MatDividerModule} from "@angular/material/divider";
-import {SuccessCardComponent} from "../../../../../shared/business/success-card/success-card.component";
-import {CommonModule} from "@angular/common";
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
+import { FullLineCardButtonComponent } from 'src/app/shared/business/full-line-card-button/full-line-card-button.component';
+import { SuccessCardComponent } from '../../../../../shared/business/success-card/success-card.component';
+import { ProfileCardComponent } from '../profile-card/profile-card.component';
 
-type Success =  {
+type Success = {
   title: string;
   value: number;
   icon: string;
   color: string;
   image: string;
-}
+};
 
 @Component({
   selector: 'app-profile-statistics',
@@ -19,32 +21,41 @@ type Success =  {
   styleUrls: ['./profile-statistics.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ProfileCardComponent,TranslateModule,MatDividerModule,SuccessCardComponent,CommonModule],
+  imports: [
+    ProfileCardComponent,
+    TranslateModule,
+    MatDividerModule,
+    SuccessCardComponent,
+    CommonModule,
+    FullLineCardButtonComponent,
+    MatIconModule,
+  ],
 })
 export class ProfileStatisticsComponent implements OnInit {
-  public successList: Success[] = [{
-    title: 'Success',
-    value: 26,
-    icon: 'check_circle',
-    color: '#00bcd4',
-    image: 'assets/images/badge_gold.svg',
-  }, {
-    title: 'Success2',
-    value: 15,
-    icon: 'check_circle',
-    color: '#00bcd4',
-    image: 'assets/images/badge_gold.svg',
-  },{
-    title: 'Success3',
-    value: 65,
-    icon: 'check_circle',
-    color: '#00bcd4',
-    image: 'assets/images/badge_gold.svg',
-  },];
-  constructor() { }
+  public successList: Success[] = [
+    {
+      title: 'Success',
+      value: 26,
+      icon: 'check_circle',
+      color: '#00bcd4',
+      image: 'assets/images/badge_gold.svg',
+    },
+    {
+      title: 'Success2',
+      value: 15,
+      icon: 'check_circle',
+      color: '#00bcd4',
+      image: 'assets/images/badge_gold.svg',
+    },
+    {
+      title: 'Success3',
+      value: 65,
+      icon: 'check_circle',
+      color: '#00bcd4',
+      image: 'assets/images/badge_gold.svg',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
