70a5e1dd2a5d34700c576151a9dc42a77f827d1c
abderrezakBKZ <59936202+abderrezakBKZ@users.noreply.github.com>
2022-12-15 13:21:54 +0100

    Rajout de la partie liste d'entrainement (#21)
    Co-authored-by: Abderrezak BOUKAZOUHA <abderrezak.bkz@gmail.com>

--- src/app/pages/train/train.page.ts
+++ src/app/pages/train/train.page.ts
@@ -2,6 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppStore } from 'src/app/app.store';
 
+const sections = [
+  {
+    number: 1,
+    description: 'Utilise des mots de base, présente-toi',
+  },
+  {
+    number: 2,
+    description: 'Dis comment tu vas, communique en voyage',
+  },
+  {
+    number: 3,
+    description: 'Commande au restaurant',
+  },
+];
+
 @Component({
   selector: 'app-train',
   templateUrl: './train.page.html',
@@ -13,7 +28,7 @@ export class TrainPage implements OnInit {
     private readonly router: Router,
     private readonly userAppStore: AppStore,
   ) {}
-
+  sectionList = sections;
   ngOnInit(): void {
     const accessToken = this.route.snapshot.queryParams['access_token'];
     if (accessToken) {
