097da8b8b68130f1d5de20c049e8bd7a35d14e49
Lounes BADJI <lounes.badji@globaz.ch>
2023-08-06 21:27:26 +0200

    fix: profile page status bar color

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { PageLayoutDirective } from '../../core/layout/directives/is-page-layout.directive';
 import { StatisticListComponent } from './components/statistic-list/statistic-list.component';
@@ -7,6 +7,7 @@ import { SuccessListComponent } from './components/success-list/success-list.com
 import { UserProfileInformationComponent } from './components/user-profile-information/user-profile-information.component';
 import { SUCCESS_ITEMS } from './components/success-list/success-items';
 import { UserProfileToolbarComponent } from './components/user-profile-toolbar/user-profile-toolbar.component';
+import { ThemeService } from '../../core/theme/theme.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -23,6 +24,12 @@ import { UserProfileToolbarComponent } from './components/user-profile-toolbar/u
   ],
   hostDirectives: [PageLayoutDirective],
 })
-export class UserProfilePage {
+export class UserProfilePage implements OnInit {
   items = SUCCESS_ITEMS;
+
+  constructor(private readonly _theme: ThemeService) {}
+
+  ngOnInit(): void {
+    this._theme.applyThemeToStatusBar();
+  }
 }
